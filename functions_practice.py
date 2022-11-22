f21d9708df717a6376ebb285a905411372107598
Ashley Vieira <ash.adr24212@gmail.com>
2022-11-21 19:25:03 -0500

    finished

--- functions_practice.py
+++ functions_practice.py
@@ -3,7 +3,24 @@ def hello():
 
 hello()
 
+
 def pack(a,b,c):
     return[a,b,c]
 
 print(pack("a","b","c"))
+
+
+def eat_lunch(my_1st):
+    if len(my_1st) == 0:
+        print("My lunchbox is empty!")
+    else:
+        for i in range(len(my_1st)):
+            if i == 0:
+                print(f"First I eat {my_1st[0]}")
+            else:
+                print(f"Next I eat {my_1st[i]}")
+
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
